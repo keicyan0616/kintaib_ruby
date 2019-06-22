914668dcb4f527a199471e846825dd9869b1c8eb
keicyan0616 <k_ken-1.0616@nifty.com>
2019-06-22 16:17:58 +0000

    勤怠B完走時点初版

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,19 +2,34 @@ class SessionsController < ApplicationController
   def new
   end
 
+  #def create
+  #  user = User.find_by(email: params[:session][:email].downcase)
+  #  if user && user.authenticate(params[:session][:password])
+  #    log_in user
+  #    remember user
+  #    redirect_back_or user
+  #  else
+  #    flash.now[:danger] = 'メールアドレスとパスワードの情報が一致しませんでした。'
+  #    render 'new'
+  #  end
+  #end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_back_or user
+      #redirect_to user
     else
       flash.now[:danger] = 'メールアドレスとパスワードの情報が一致しませんでした。'
-      render 'new'
+      render :new
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
+    flash[:success] = 'ログアウトしました。'
     redirect_to root_url
   end
 end
